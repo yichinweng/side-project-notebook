35680bda509106d674bf8a3d5db03cc3bb7a3218
Winter Weng <yichinweng@gmail.com>
2017-07-16 19:15:22 +0800

    Add 'Content' component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import List from '../List/List';
+import Content from '../Content/Content';
 
 class App extends React.Component {
   render() {
     return (
-      <p>
-        App test
-      </p>
+      <div>
+        <Content />
+      </div>
     )
   }
 }
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,1 +1,11 @@
-// content
+import React from 'react';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <p>Note Book Content</p>
+    )
+  }
+}
+
+export default Content;
