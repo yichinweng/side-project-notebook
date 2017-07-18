d6d100683a15a6a832bdff28555c177b0fb8ff2a
Winter Weng <yichinweng@gmail.com>
2017-07-18 20:26:03 +0800

    Add state into app, and add update note function.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,29 @@ import List from '../List/List';
 import Content from '../Content/Content';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.updateNote = this.updateNote.bind(this);
+    this.state = {
+      notes: {
+        noteDefault: {
+          title: 'Untitled',
+          content: '',
+          timestamp: Date.now(),
+        },
+      },
+    };
+  }
+  updateNote(key, updatedNote) {
+    const notes = { ...this.state.notes };
+    notes[key] = updatedNote;
+    this.setState({ notes });
+  }
   render() {
     return (
       <div>
-        <Content />
+        <List notes={this.state.notes} />
+        <Content notes={this.state.notes} updateNote={this.updateNote} />
       </div>
     );
   }
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class Content extends React.Component {
+  constructor() {
+    super();
+    this.saveNote = this.saveNote.bind(this);
+  }
+  saveNote(e, key) {
+    const notes = this.props.notes[key];
+    const updatedNotes = {
+      ...notes,
+      [e.target.name]: e.target.value,
+      timestamp: Date.now(),
+    };
+    this.props.updateNote(key, updatedNotes);
+  }
   render() {
     return (
-      <p>Note Book Content</p>
+      <div>
+        <textarea
+          key="noteDefault"
+          name="content"
+          placeholder="Enter your note..."
+          onChange={e => this.saveNote(e, 'noteDefault')}
+        />
+      </div>
     );
   }
 }
