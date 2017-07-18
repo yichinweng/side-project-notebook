712c6836662694584daf241894b74f89022c091d
wind0304598 <wnid0304598@gmail.com>
2017-07-18 20:30:01 +0800

    Update list component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,1 +1,28 @@
-// list
+import React from 'react';
+
+class List extends React.Component {
+  renderNotebook(key) {
+    const note = this.props.notes[key];
+    return (
+      <div className="notebook-inside-list" key={key}>
+        <h2>{note.title}</h2>
+        <h2>{note.timestamp}</h2>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>筆記本</h1>
+        {
+          Object
+            .keys(this.props.notes)
+            .map(key => this.renderNotebook(key))
+        }
+      </div>
+    );
+  }
+}
+
+export default List;
