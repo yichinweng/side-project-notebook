b2725116610d62a4bcd1ae4746f57134f2edfa82
Winter Weng <yichinweng@gmail.com>
2017-09-16 22:25:51 +0800

    remove firebase.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import List from '../List/List';
 import Content from '../Content/Content';
 import { getDefaultNote } from '../../helper';
-import base from '../../base';
 
 import './App.scss';
 
@@ -20,16 +19,6 @@ class App extends React.Component {
       ],
       focus: 0,
     };
-  }
-  componentWillMount() {
-    // this runs right before the <App> is rendered
-    this.ref = base.syncState('notes', {
-      context: this,
-      state: 'notes',
-    });
-  }
-  componentWillUnmount() {
-    base.removeBinding(this.ref);
   }
   updateNote(key, updatedNote) {
     const notes = [...this.state.notes];
